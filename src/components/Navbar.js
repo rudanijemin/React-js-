b9bc0741b38613ca1fea0895f653edbfc1dd9229
jemin <rudanijemin18@gmail.com>
2022-07-24 18:25:15 +0530

    Props and PropTypes

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Navbar(props) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="/">{props.title}</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link " aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">{props.aboutText}</a>
+          </li>
+         
+          
+        </ul>
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+
+//this is a proptypes
+// title: PropTypes.string means title always string jj write karvu bhulthi nunmer hse to eror return thse
+Navbar.propTypes={
+  title: PropTypes.string,
+  aboutText:PropTypes.string
+}
+//defaultprops koi value pass nai thai hoii to default je value hse print thse
+Navbar.defaultProps={
+  title:"TextUtils",
+  aboutText:"AboutUS.."
+
+};
